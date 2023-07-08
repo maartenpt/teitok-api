6d7844cb371bf72dda53c25a69124d0d34611bd6
Maarten Janssen <maartenpt@gmail.com>
2023-07-08 14:53:50 +0200

    added backup script

---
+++ Python/apistart.py
@@ -0,0 +1,44 @@
+import argparse
+import os
+import re
+
+# Default header to get the URL and token for the project
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-t", "--token", help="Authorization token")
+argParser.add_argument("-u", "--url", help="TEITOK corpus project URL")
+argParser.add_argument("-d", "--settings", help="Settings file")
+argParser.add_argument("-c", "--corpus", help="Corpus ID")
+argParser.add_argument("-s", "--silent", help="silent mode", action='store_true')
+args = argParser.parse_args()
+
+curdir = os.path.basename(os.getcwd())
+inifile = args.settings or "./config.ini"
+
+corpus = args.corpus or "DEFAULT"
+
+# Emulate configparse
+config = {}
+if os.path.exists(inifile):
+    sts = "DEFAULT"
+    config[sts] = {}
+    with open(inifile, "r") as file:
+        for line in file:
+            line = line.strip()
+            m1 = re.match(r"^\[(.*)\]$", line)
+            m2 = re.match(r"^(.*?) = (.*)$", line)
+            if m1:
+                sts = m1.group(1)
+                if not sts in config.keys():
+                    config[sts] = {}
+            elif m2:
+                config[sts][m2.group(1)] = m2.group(2)
+
+apiurl = args.url or config[corpus]["url"]
+token = args.token or config[corpus]["token"]
+
+if not apiurl:
+    print("Please provide an API URL")
+    exit()
+if not token:
+    print("Please provide an authorization token")
+    exit()
---
+++ Python/backup.py
@@ -0,0 +1,45 @@
+import glob, os, requests, sys, re
+from pathlib import Path
+from datetime import datetime
+import urllib.parse
+
+# Load the configuration settings
+dir = os.path.dirname(sys.argv[0])
+sys.path.append(dir)
+import apistart
+apiurl = apistart.apiurl + "?action=api"
+token = apistart.token
+
+print("Using TEITOK project: " + apiurl)
+
+# The URL to use to upload an RTF file
+listurl = apiurl + "&act=list&token="+token
+
+if os.path.exists("lastbu.txt"):
+    with open("lastbu.txt", "r") as file:
+         last = file.read().strip()
+    print(" - backing up new files since " + last)
+    listurl = listurl + "&since=" + urllib.parse.quote_plus(last)
+        
+# Get the list of all the files on the server
+response = requests.get(listurl) # Upload the file
+try:
+    list = response.json()
+except:
+    print('Error in request: ' + response.text)
+    exit()
+  
+if "error" in list.keys():
+    print('Error in request: ' + response.text)
+    print (listurl)	
+    exit()
+    
+for filename in list['files']:
+    print(filename)
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    response = requests.get( apiurl + "&act=download&cid="+filename+"&token="+token)
+    with open(filename, "w") as f:
+        f.write(response.text)    
+        
+with open("lastbu.txt", "w") as f:
+	f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))    
