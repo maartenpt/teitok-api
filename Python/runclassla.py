6cb7d92c6d530b19f40b5f484cf51592b6c95f06
Maarten Janssen <maartenpt@gmail.com>
2023-07-14 13:39:34 +0200

    made runclassla.py properly use apistart

--- Python/runclassla.py
+++ Python/runclassla.py
@@ -8,12 +8,10 @@ dir = os.path.dirname(sys.argv[0])
 sys.path.append(dir)
 import apistart
 apiurl = apistart.apiurl + "?action=api"
-token = apistart.token
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-m", "--model", help="CLASSLA model", default="mk")
 args, moreargs = argParser.parse_known_args()
-
 
 model = args.model
 
@@ -24,9 +22,9 @@ classla.download("mk")
 nlp = classla.Pipeline(lang='mk', tokenize_pretokenized='conllu')
 
 # The URL to use to upload an RTF file
-listurl = apiurl + "&act=list&token="+token
+listurl = apiurl + "&act=list"
 
-response = requests.get(listurl) # Upload the file
+response = requests.get(listurl, cookies=apistart.cookies) # Upload the file
 try:
     data = response.json()
 except:
@@ -38,17 +36,19 @@ print(str(filecnt) + " files to treat")
 i = 0
 
 for file in data["files"]:
-    i = i = 1
+    i = i + 1
     file = file.split("/")[-1]
     print(str(i) + ": " + file)
     conf = file.replace('.xml', '.conllu')
     tagf = file.replace('.xml', '-tagged.conllu')
-    resp = requests.get(apiurl+"&act=download&format=conllu&token="+token+"&cid="+file)
+    resp = requests.get(apiurl+"&act=download&format=conllu&cid="+file, cookies=apistart.cookies)
     conllu_pretokenized = resp.text
     if conllu_pretokenized.strip() == "":
-        print("no CoNLL-U result rendered")
+        print("no CoNLL-U result rendered (not tokenized?)")
     else:
         doc = nlp(conllu_pretokenized)
         with open(tagf, 'w') as f: f.write(doc.to_conll())
-        cmd = "curl -F 'infile=@"+tagf+"' '"+apiurl+"&act=annotate&format=conllu&token="+token+"&cid="+file+"'"
-        os.system(cmd)
+        files = {'infile': open(tagf, 'rb')}
+        upload_url = apiurl+"&act=annotate&format=conllu&cid="+file
+        response = requests.post(upload_url, cookies=apistart.cookies, files=files) # Upload the file
+        print(response.text)
